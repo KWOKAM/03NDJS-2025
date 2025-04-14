163505250a8d134b5635061b66bff46556efb0f0
KWOKAM <ronald.tatouk97@gmail.com>
2025-04-14 17:36:59 +0200

    main.js

---
+++ Day-1/sandbox/first_step/math.js
@@ -0,0 +1,19 @@
+// math.js
+function sum(a, b) {
+  return a + b;
+}
+
+function diff(a, b) {
+  return a - b;
+}
+
+function quot(a, b) {
+  return a / b;
+}
+
+function prod(a, b) {
+  return a * b;
+}
+
+// Export the functions
+module.exports = { sum, diff, quot, prod };
