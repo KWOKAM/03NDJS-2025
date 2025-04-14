9ae9bce678491016eb1cc8f415c19bc8fd6ccc14
KWOKAM <ronald.tatouk97@gmail.com>
2025-04-14 17:22:04 +0200

    main.js

---
+++ Day-1/sandbox/first_step/main.js
@@ -0,0 +1,7 @@
+const { sum, diff, quot, prod } = require('./math');
+
+console.log('Addition:', sum(4, 2));         // 6
+console.log('Soustraction:', diff(4, 2));    // 2
+console.log('Division:', quot(4, 2));        // 2
+console.log('Multiplication:', prod(4, 2));   // 8
+
